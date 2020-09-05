eba25d7e0fcd78bf0c2a9f5466741cdc76b17754
okateru <okateru.0217@gmail.com>
2020-09-05 14:59:35 +0900

    vue_1

---
+++ vue_project_1/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>
+      {{msg}}
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      msg: 'Hello World!!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
